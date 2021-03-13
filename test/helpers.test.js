b5a1222110c50371f26550ccb97c9169bd7a7f55
ParamagicDev <konnor5456@gmail.com>
2021-03-13 10:36:37 -0500

    chore: package.json cleanup and update test suite
    chore: package.json cleanup, and update test suite
    
    chore: remove husky
    
    chore: add husky
    
    chore: add husky
    
    remove const
    
    chore: add husky
    
    remove husky prepare hook
    
    fix tests

--- test/helpers.test.js
+++ test/helpers.test.js
@@ -1,15 +1,17 @@
 /* eslint-env mocha */
 
-import expect from 'expect'
+import chai from 'chai'
 import { REQUIRED_FIELDS } from '../src/constants'
 import * as helpers from '../src/helpers'
+
+const expect = chai.expect
 
 describe('helpers', function () {
   describe('handleError', function () {
     it('should return an array of length 1 given a single error', function () {
       const result = helpers.handleError(new Error('required field missing'))
-      expect(result).toBeA(Array)
-      expect(result.length).toEqual(1)
+      expect(result).to.be.an.instanceof(Array)
+      expect(result.length).to.eq(1)
     })
 
     it('should return an array of length 2 given an array of length 2', function () {
@@ -17,14 +19,14 @@ describe('helpers', function () {
         new Error('required field missing'),
         new Error('request failed')
       ])
-      expect(result).toBeA(Array)
-      expect(result.length).toEqual(2)
+      expect(result).to.an.instanceof(Array)
+      expect(result.length).to.eq(2)
     })
 
     it('should prefix message of single error', function () {
       const result = helpers.handleError(new Error('required field missing'), 'Plugin')
-      expect(result.length).toEqual(1)
-      expect(result[0]).toInclude({ message: 'Plugin: required field missing' })
+      expect(result.length).to.eq(1)
+      expect(result[0]).to.include({ message: 'Plugin: required field missing' })
     })
 
     it('should prefix message of an array of errors', function () {
@@ -32,26 +34,28 @@ describe('helpers', function () {
         new Error('required field missing'),
         new Error('request failed')
       ], 'Plugin')
-      expect(result.length).toEqual(2)
-      expect(result[0]).toInclude({ message: 'Plugin: required field missing' })
+      expect(result.length).to.eq(2)
+      expect(result[0]).to.include({ message: 'Plugin: required field missing' })
     })
 
     it('should default prefix to "HoneybadgerSourceMapPlugin"', function () {
       const result = helpers.handleError(new Error('required field missing'))
-      expect(result.length).toEqual(1)
-      expect(result[0]).toInclude({
+      expect(result.length).to.eq(1)
+      expect(result[0]).to.include({
         message: 'HoneybadgerSourceMapPlugin: required field missing'
       })
     })
 
     it('should handle null', function () {
       const result = helpers.handleError(null)
-      expect(result).toEqual([])
+      expect(result).to.be.an('array')
+      expect(result).to.be.empty
     })
 
     it('should handle empty []', function () {
       const result = helpers.handleError([])
-      expect(result).toEqual([])
+      expect(result).to.be.an('array')
+      expect(result).to.be.empty
     })
   })
 
@@ -63,7 +67,7 @@ describe('helpers', function () {
         assetsUrl: 'https://cdn.example.com/assets'
       }
       const result = helpers.validateOptions(options)
-      expect(result).toBe(null) // eslint-disable-line no-unused-expressions
+      expect(result).to.eq(null)
     })
 
     it('should return an error if apiKey is not supplied', function () {
@@ -72,10 +76,10 @@ describe('helpers', function () {
         assetsUrl: 'https://cdn.example.com/assets'
       }
       const result = helpers.validateOptions(options)
-      expect(result).toBeA('array')
-      expect(result.length).toBe(1)
-      expect(result[0]).toBeA(Error)
-        .toInclude({ message: 'required field, \'apiKey\', is missing.' })
+      expect(result).to.be.an.instanceof(Array)
+      expect(result.length).to.eq(1)
+      expect(result[0]).to.be.an.instanceof(Error)
+      expect(result[0]).to.include({ message: 'required field, \'apiKey\', is missing.' })
     })
 
     it('should return an error if assetsUrl is not supplied', function () {
@@ -84,29 +88,29 @@ describe('helpers', function () {
         revision: 'fab5a8727c70647dcc539318b5b3e9b0cb8ae17b'
       }
       const result = helpers.validateOptions(options)
-      expect(result).toBeA('array')
-      expect(result.length).toBe(1)
-      expect(result[0]).toBeA(Error)
-        .toInclude({ message: 'required field, \'assetsUrl\', is missing.' })
+      expect(result).to.be.an.instanceof(Array)
+      expect(result.length).to.eq(1)
+      expect(result[0]).to.be.an.instanceof(Error)
+      expect(result[0]).to.include({ message: 'required field, \'assetsUrl\', is missing.' })
     })
 
     it('should handle multiple missing required options', function () {
       const options = {}
       const result = helpers.validateOptions(options)
-      expect(result).toBeA(Array)
-      expect(result.length).toBe(REQUIRED_FIELDS.length)
+      expect(result).to.be.an.instanceof(Array)
+      expect(result.length).to.eq(REQUIRED_FIELDS.length)
     })
 
     it('should handle null for options', function () {
       const result = helpers.validateOptions(null)
-      expect(result).toBeA(Array)
-      expect(result.length).toBe(REQUIRED_FIELDS.length)
+      expect(result).to.be.an.instanceof(Array)
+      expect(result.length).to.eq(REQUIRED_FIELDS.length)
     })
 
     it('should handle no options passed', function () {
       const result = helpers.validateOptions()
-      expect(result).toBeA(Array)
-      expect(result.length).toBe(REQUIRED_FIELDS.length)
+      expect(result).to.be.an.instanceof(Array)
+      expect(result.length).to.eq(REQUIRED_FIELDS.length)
     })
   })
 })
